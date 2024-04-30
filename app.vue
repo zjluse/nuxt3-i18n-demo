5a98cb12802381a3b1f6dbcc0a706032d4bbcddd
二小 <1539639526@qq.com>
2024-04-30 17:57:26 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <button @click="setLocale('en')">en</button> |
+      <button @click="setLocale('fr')">fr</button>
+    </div>
+    <p>{{ $t('welcome') }}</p>
+    <p>{{ text }}</p>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const { t, setLocale } = useI18n();
+
+const text = ref(t('welcome'))
+</script>
